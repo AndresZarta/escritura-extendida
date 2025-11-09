3e8653d35244f079b75c9c7f09c0da36f137cd0a
deleuzianscript.sh@gmail.com <andresnico94@hotmail.com>
2025-11-09 16:53:58 -0500

    Change image directory

--- public/js/demo-button.js
+++
@@ -1,7 +0,0 @@
-const btn = document.getElementById('boton-demo');
-if (btn) {
-  btn.addEventListener('click', () => {
-    btn.style.backgroundColor = btn.style.backgroundColor === 'crimson' ? '' : 'crimson';
-    btn.style.color = btn.style.color === 'white' ? '' : 'white';
-  });
-}
