8f03264f8435a2b7ac4859e079be6dc7b58af2e4
deleuzianscript.sh@gmail.com <andresnico94@hotmail.com>
2025-11-09 16:18:53 -0500

    Add base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://AndresZarta.github.io',
+  base: '/escritura-extendida',
   integrations: [mdx()],
   markdown: {
     // Example: switch highlight type or theme
